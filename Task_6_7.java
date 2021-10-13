cbb86c90448722b578183c101c1c71f1c7812cc2
AveBar <92439959+AveBar@users.noreply.github.com>
2021-10-13 15:14:12 +0300

    Create Task_6_7.java

---
+++ Task_6_7.java
@@ -0,0 +1,26 @@
+package end;
+
+public class Task_6 {
+    public static void main(String[] args) {
+        Field field = Field.createField();
+        field.summation();
+//        field.subtraction();
+//        field.multiply();
+//        field.division();
+//        field.degree();
+//        field.multiplicativeInversion();
+//        field.division();
+    }
+  public class Task_7 {
+    public static void main(String[] args) {
+        Field_BigInteger field = Field_BigInteger.createField();
+//        field.summation();
+//        field.subtraction();
+//        field.multiply();
+//        field.division();
+//        field.degree();
+//        field.multiplicativeInversion();
+        field.division();
+    }
+
+}
